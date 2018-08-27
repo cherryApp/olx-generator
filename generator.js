53835beb19cf98e6623db380cd34dcdb331bdb0e
Cserkó József <j.cserko@gmail.com>
2018-08-27 11:39:41 +0200

    Feat: add xml generator.

--- generator.js
+++ generator.js
@@ -13,6 +13,14 @@ if (!args.url) {
 // Use this with cookie hack.
 // node generator -u 'https://yellowroad.training360.com/lesson/basic/week2/02_char_coding' -head 'cookie|YellowSSO=B9pUg7YBQFdvKY5nKutfgiWgh0VWf0qkDa68Dp0XTrNqGjhm3DgD8tT9vGRnaRTbAi/WqqJ3nH10saiGz4GoyYTmGGHKMF0HARQ7FVLX3cip4h8A49djwTUqGim04UJncw2R87cmm0jOnTSnbaYkxnbPJ4L1c+MT4iwqIIrt/q1kqsejGp24x0ONCgIawEfEw+0HHLE9HDU2TqeE+NndFld8KIUp5uwrcCxUdmLO/Uvpfv5UvxE7VY9CGjTgRxQjYWNVe7w1AaGKD3GtokSNEQ==;'
 
+// Init course.
+let courseData = {
+    "url_name": "course",
+    "org": "edX",
+    "course": "DemoX"    
+};
+const course = new OlxGenerator(courseData);
+/*
 // Get content from the url.
 const client = got.extend({
     baseUrl: args.url,
@@ -29,3 +37,4 @@ const processContent = data => {
     let body = data.body;
     let gen = new OlxGenerator(body);
 };
+*/
--- module/olxGenerator.js
+++ module/olxGenerator.js
@@ -1,9 +1,13 @@
-const cheerio = require('cheerio');
+const cheerio = require('cheerio'),
+    builder = require('xmlbuilder');
 
 module.exports = class Generator {
-    constructor(siteData) {
+    constructor(courseData) {
+        /*
         this.siteData = siteData;
         this.$ = cheerio.load(this.siteData);
+        */
+        this.courseData = courseData;
         this.init();
     }
 
@@ -12,7 +16,12 @@ module.exports = class Generator {
     }
 
     createCourse() {
-        let title = this.$('title').text();
-        
+        // let title = this.$('title').text();
+        let xml = builder.create('course');
+        for (let k in this.courseData) {
+            xml.att(k, this.courseData[k]);
+        }
+        xml.end({pretty: true});
+        console.log(xml.toString());
     }
 };
